d4412c65e81db21d6af20ba5ae990a01c1aee0fb
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2013-12-11 11:12:26 +0000

    Updated for Bennu 2

---
+++ src/main/java/pt/ist/fenix/webapp/JvstmOjbConnectionManager.java
@@ -0,0 +1,22 @@
+package pt.ist.fenix.webapp;
+
+import java.sql.Connection;
+
+import net.sourceforge.fenixedu.util.ConnectionManager;
+
+import org.apache.ojb.broker.accesslayer.LookupException;
+
+import pt.ist.fenixframework.backend.jvstmojb.pstm.TransactionSupport;
+
+public class JvstmOjbConnectionManager extends ConnectionManager {
+
+    @Override
+    protected Connection getConnection() {
+        try {
+            return TransactionSupport.getOJBBroker().serviceConnectionManager().getConnection();
+        } catch (LookupException e) {
+            throw new Error(e);
+        }
+    }
+
+}
