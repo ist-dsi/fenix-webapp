85594bf258f80ae7d961749b3a01cc5d36ce63f8
Luis Cruz <luis.cruz@tecnico.pt>
2021-10-29 15:06:02 +0100

    Script for creating events

---
+++ src/main/java/pt/ist/fenix/webapp/task/CreateEventsFromTemplate.java
@@ -0,0 +1,27 @@
+package pt.ist.fenix.webapp.task;
+
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.accounting.EventTemplate;
+import org.fenixedu.academic.domain.student.RegistrationDataByExecutionYear;
+import org.fenixedu.bennu.scheduler.custom.ReadCustomTask;
+import pt.ist.fenixframework.FenixFramework;
+
+public class CreateEventsFromTemplate extends ReadCustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        ExecutionYear.readCurrentExecutionYear().getRegistrationDataByExecutionYearSet().stream()
+                .parallel()
+                .forEach(this::process);
+    }
+
+    private void process(final RegistrationDataByExecutionYear dataByExecutionYear) {
+        FenixFramework.atomic(() -> {
+            final EventTemplate eventTemplate = EventTemplate.templateFor(dataByExecutionYear);
+            if (eventTemplate != null) {
+                eventTemplate.createEventsFor(dataByExecutionYear);
+            }
+        });
+    }
+
+}
